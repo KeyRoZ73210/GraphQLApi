95ce957df0e6a1f94cc9c23ba4393593f4216bcb
thomas.fleury73 <thomas.fleury73@gmail.com>
2024-03-22 10:45:21 +0100

    [V1]

--- backend/src/models/Comment.js
+++ backend/src/models/Comment.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const commentSchema = new mongoose.Schema({
     content: String,
     author: String,
-    postId: String
+    postId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post"  // Référence au modèle Post, assurez-vous que c'est le même que celui que vous avez défini pour votre modèle Post
+    }
 
 } , {
   timestamps: true
